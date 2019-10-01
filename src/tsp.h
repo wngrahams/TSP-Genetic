f38d98c2da526aa55bc5a8816d6c5ce539b7ea4a
wngrahams <w.grahamstubbs@gmail.com>
2019-10-01 18:30:25 -0400

    fixed tabs

--- src/tsp.h
+++ src/tsp.h
@@ -19,15 +19,15 @@
 #define GREATER_THAN 1
 
 struct point {
-	double x;
-	double y;
+    double x;
+    double y;
 };
 
 /*
  * Calculates the euclidean distance between two points
  */
 static inline double calc_dist(const struct point* p1, const struct point* p2){
-	return hypot(p2->x - p1->x, p2->y - p1->y);
+    return hypot(p2->x - p1->x, p2->y - p1->y);
 }
 
 /* 
@@ -35,19 +35,19 @@ static inline double calc_dist(const struct point* p1, const struct point* p2){
  * prints an error message. The function returns 1 on success and 0 on failure
  */ 
 int8_t check_malloc_err(const void *ptr) {
-	if (NULL == ptr) {
-		perror("malloc() returned NULL");
-		return 0;         
-	} /* END if */
+    if (NULL == ptr) {
+        perror("malloc() returned NULL");
+        return 0;         
+    } /* END if */
 
-	return 1;
+    return 1;
 }
 
 /*
  * Evaluates if lhs is less than or greater than rhs depending on flag passed
  */
 static inline int8_t lt_gt(const double lhs, const double rhs, const int symb) {
-	return ((symb == LESS_THAN) ? (lhs < rhs) : (lhs > rhs));
+    return ((symb == LESS_THAN) ? (lhs < rhs) : (lhs > rhs));
 }
 
 #endif /* _TSP_H_ */
